7f020cf73fe84a7bed74665ce319067f8fa6c88e
Rothanak So <rothanak.hello@gmail.com>
2020-01-10 08:12:40 -0500

    Golang, Snap, AI GPU server

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,10 @@ status --is-interactive; and source (nodenv init -|psub)
 # Python (beancount and fava)
 set -x PATH $PATH $HOME/.local/bin
 
+# Go
+set -x -U GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
+
 # Anaconda
 set -x PATH $PATH $HOME/anaconda3/bin
 source (conda info --root)/etc/fish/conf.d/conda.fish
@@ -19,6 +23,9 @@ set -x PATH $PATH $HOME/flutter/bin
 
 # Android
 set -x PATH $PATH $HOME/Android/Sdk/platform-tools
+
+# Snap
+set -x PATH $PATH /var/lib/snapd/snap/bin
 
 # Disable middle click paste
 xmodmap -e "pointer = 1 25 3 4 5 6 7 8 9" 2>/dev/null
