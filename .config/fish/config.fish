75697037e97ab0e3a810444c6f71ff082548366b
Rothanak So <rothanak.hello@gmail.com>
2025-08-03 19:42:44 -0400

    Conditionally run commands

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,7 @@ set -x JAVA_HOME /usr/lib/jvm/java
 
 # Nodenv
 set -x PATH $PATH $HOME/.nodenv/bin
-status --is-interactive; and source (nodenv init -|psub)
+type -q nodenv; and status --is-interactive; and source (nodenv init -|psub)
 
 # Python (beancount and fava)
 set -x PATH $PATH $HOME/.local/bin
@@ -16,7 +16,7 @@ set -x PATH $PATH $GOPATH/bin /usr/local/go/bin
 
 # Anaconda
 set -x PATH $PATH $HOME/anaconda3/bin
-source (conda info --root)/etc/fish/conf.d/conda.fish
+type -q conda; and source (conda info --root)/etc/fish/conf.d/conda.fish
 
 # Flutter
 set -x PATH $PATH $HOME/flutter/bin
@@ -28,7 +28,7 @@ set -x PATH $PATH $HOME/Android/Sdk/platform-tools
 set -x PATH $PATH /var/lib/snapd/snap/bin
 
 # Disable middle click paste
-xmodmap -e "pointer = 1 25 3 4 5 6 7 8 9" 2>/dev/null
+type -q xmodmap; and xmodmap -e "pointer = 1 25 3 4 5 6 7 8 9" 2>/dev/null
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/rothanak/google-cloud-sdk/path.fish.inc' ]; . '/home/rothanak/google-cloud-sdk/path.fish.inc'; end
